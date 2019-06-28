5e988127e4d847e717755a05ff4f696e7c6b3068
Tyler <tjk6@students.uwf.edu>
2019-06-28 12:59:04 -0500

    everything works
    we did it
    wow
    such amaze

--- Dual_Drapery_Demo.lua
+++ Dual_Drapery_Demo.lua
@@ -1,7 +1,7 @@
 --| Initialization ----------------------------------------------------------------
     function EDRV_Init()
     -- called when the driver starts up
-		HOST = "testing.currentproducts.io"
+		HOST = "api.currentproducts.io"
 		CLIENT_ID = "ELANdriver"
 		CLIENT_SECRET = "ELANsecret"
 		local sAuthURL = "https://" .. HOST .. "/oauth/authorize?"
@@ -17,6 +17,7 @@
 	Is called when a user hits authorize
 --]]-------------------------------------------------------
 	function EDRV_RecvOAuthorizationCode( sAuthCode )
+		--create headers and body
 		local sHTTP = "POST /oauth/token HTTP/1.1\r\n"
 					.. "Accept: application/json\r\n"
 					.. "Host: " .. HOST .. "\r\n"
@@ -28,13 +29,12 @@
 					.. "&client_id=" .. CLIENT_ID
 					.. "&client_secret=" .. CLIENT_SECRET
 					.. "&redirect_uri=" .. ELAN_GetOAuthRedirectURI()
+		--socket interaction
+		local socket = ELAN_CreateTCPClientSocket(HOST,80) --TODO: make this SSLClientSocket(?)
+		local isConnected = ELAN_ConnectTCPSocket(socket)
 
-		local SSL_socket = ELAN_CreateTCPClientSocket(HOST,80) --TODO: make this SSLClientSocket
-		ELAN_Trace(string.format("Connecting to %s:%d", HOST,80))
-
-		local isConnected = ELAN_ConnectTCPSocket(SSL_socket)
-
-		local response = ELAN_DoHTTPExchange(SSL_socket, sHTTP, sContent, true, 5000)
+		--response validation
+		local response = ELAN_DoHTTPExchange(socket, sHTTP, sContent, true, 5000)
 		local p1, p2
 		p1,p2 = response:find("200 OK")
 		
@@ -42,26 +42,24 @@
 		ELAN_Trace(string.format("Response: %s", response))
 
 		if(p1 ~= nil) then
+			--response parsing
 			ELAN_Trace(string.format("p1: %s, p2: %s", tostring(p1), tostring(p2)))			
 
 			local hJSON = ELAN_CreateJSONMsg(response)
-			ELAN_Trace(string.format("hJSON: %s", tostring(hJSON)))
 
 			sAccessToken = ELAN_FindJSONValueByKey(hJSON, hJSON, "access_token")
 			ELAN_SaveOAuthAccessToken(sAccessToken)
-			ELAN_Trace(string.format("sAccessToken: %s", tostring(sAccessToken)))
 
 			sRefreshToken = ELAN_FindJSONValueByKey(hJSON, hJSON, "refresh_token")
 			ELAN_SaveOAuthRefreshToken(sRefreshToken)
-			ELAN_Trace(string.format("sRefreshToken: %s", tostring(sRefreshToken)))
 
 			iExpiration = ELAN_FindJSONValueByKey(hJSON, hJSON, "expires_in")
 			ELAN_SetOAuthTokenExpiration(iExpiration)
-			ELAN_Trace(string.format("iExpiration: %s", tostring(iExpiration)))
-
-			DeviceDiscovery(SSL_socket)
+			
+			--Populates the Lighting interface with devices from the server
+			DeviceDiscovery(socket)
 		else
-			ELAN_CloseSocket(SSL_socket)
+			ELAN_CloseSocket(socket)
 		end
 	end
 
@@ -70,7 +68,7 @@
 	@return boolean for refresh success
 --]]-------------------------------------------------------
 	function checkTokenExpired()
-		if (ELAN_getOAuthTokenTTL() <= 0) then
+		if (ELAN_GetOAuthTokenTTL() <= 0) then
 			--create headers and body
 			local sHTTP = "POST /oauth/token HTTP/1.1\r\n"
 						.. "Accept: application/json\r\n"
@@ -89,6 +87,9 @@
 			local response = ELAN_DoHTTPExchange(socket, sHTTP, sContent, true, 5000)
 			local p1, p2
 			p1,p2 = response:find("200 OK")
+
+			ELAN_Trace(string.format("Response: %s", response))
+
 			if(p1 ~= nil) then
 				--response parsing
 				local hJSON = ELAN_CreateJSONMsg(response)
@@ -99,7 +100,7 @@
 				iExpiration = ELAN_FindJSONValueByKey(hJSON, hJSON, "expires_in")
 				ELAN_SetOAuthTokenExpiration(iExpiration)
 			else
-				ELAN_CloseSocket(SSL_socket)
+				ELAN_CloseSocket(socket)
 				--socket comms failed
 				return false
 			end
@@ -125,51 +126,76 @@
 					.. "Content-Type: application/x-www-form-urlencoded\r\n"
 					.. "\r\n"
 		local response = ELAN_DoHTTPExchange(socket, sHTTP, 5000)
-		ELAN_Trace(response)
+		ELAN_Trace(string.format("Response: %s", response))
 		local devicesJSON = ELAN_CreateJSONMsg(response)
 		
-
 		local deviceCount = ELAN_GetJSONSubNodeCount(devicesJSON, devicesJSON )
-		ELAN_Trace(string.format("Device Count:  %d",deviceCount))
 		local index = 0
+
+		--Search through devices on server and add them to the configurator
 		for index = 0, deviceCount-1 do
 			local deviceItemJSON = ELAN_GetJSONSubNode(devicesJSON, devicesJSON,index)
-			local deviceID = ELAN_GetJSONValue(devicesJSON,deviceItemJSON)
-			ELAN_Trace(string.format("JSON: %s",tostring(deviceID)))
-			local numSubNodes = ELAN_GetJSONSubNodeCount(devicesJSON, deviceItemJSON)
-			ELAN_Trace(string.format("Number of subnodes: %d",numSubNodes))			
+			local deviceID = ELAN_GetJSONValue(devicesJSON,deviceItemJSON)		
 			local deviceName = ELAN_FindJSONValueByKey(devicesJSON, deviceItemJSON, "name")
 			local deviceType = ELAN_FindJSONValueByKey(devicesJSON, deviceItemJSON, "type")
 			local deviceNumMotors = ELAN_FindJSONValueByKey(devicesJSON, deviceItemJSON, "numMotors")
+			--Create a second device if the decvice has two motors. This will append the appropriate motor number to the device name for lookup.
 			if deviceNumMotors > 1 then
-				ELAN_AddLightingDevice("DIMMER_MULTI_CH",deviceName,deviceID,deviceType)
-			else 
-				ELAN_AddLightingDevice("DIMMER",deviceName,deviceID,deviceType)
+				ELAN_AddLightingDevice("DIMMER",deviceName .. " Motor 1",deviceID,deviceType,"sheer")
+				ELAN_AddLightingDevice("DIMMER",deviceName .. " Motor 2",deviceID,deviceType,"blackout")
+			else
+				ELAN_AddLightingDevice("DIMMER",deviceName,deviceID,deviceType,"blackout")
 			end
-			ELAN_Trace(string.format("JSON: %s",tostring(devicesJSON)))
-			ELAN_Trace(string.format("Name: %s",tostring(deviceName)))
-			ELAN_Trace(string.format("Type: %s",tostring(deviceType)))
-			ELAN_Trace(string.format("NumMotors:  %d",deviceNumMotors))
 		end
+		ELAN_CloseSocket(socket)
 	end
 
-	function EDRV_ProcessIncoming(data)
-		ELAN_Trace(string.format("Data: %s",tostring(data)))
+--| System Calls ------------------------------------------------------------------
+--[[-------------------------------------------------------
+	Called when the slider is set to a new position
+--]]-------------------------------------------------------
+	function EDRV_DimDeviceTo(data, deviceID, deviceType, motor)
+		SetPosition(deviceID, data, motor)
 	end
-
-    function EDRV_ExecuteFunction(funcName)
-        if (funcName == "Sample Command 1") then
-            -- Execute Command 1
-        end
-
-        if (funcName == "Sample Command 2") then
-            -- Execute Command 2
-        end
-
-        if (funcName == "Sample Command 3") then
-            -- Execute Command 3
-        end
-    end
-
-
-
+--[[-------------------------------------------------------
+	Called when the slider is set to closed
+--]]-------------------------------------------------------
+	function EDRV_ActivateDevice(deviceID, deviceType, motor)
+		SetPosition(deviceID, 100, motor)
+	end
+--[[-------------------------------------------------------
+	Called when the slider is set to open
+--]]-------------------------------------------------------
+	function EDRV_DeactivateDevice(deviceID, deviceType, motor)
+		SetPosition(deviceID, 0, motor)
+	end
+--[[-------------------------------------------------------
+	HTTP call for setting the new position
+--]]-------------------------------------------------------
+	function SetPosition(deviceID, position, motor)
+		--First, check and see if the token is still valid. Get a new one if it is.
+		local recievedToken = checkTokenExpired()
+		
+		if not(recievedToken) then
+			ELAN_Trace("ERROR: Token could not be refreshed")
+			return
+		end
+		local sHTTP = "POST /v1/position/reduced HTTP/1.1\r\n"
+					.. "Accept: application/json\r\n"
+					.. "Host: " .. HOST .. "\r\n"
+					.. "Authorization: Bearer " .. ELAN_GetOAuthAccessToken() .. "\r\n"
+					.. "Content-Type: application/json\r\n"
+					.. "\r\n"
+		local socket = ELAN_CreateTCPClientSocket(HOST,80) --TODO: make this SSLClientSocket(?)
+		local JSONMsg = ELAN_CreateJSONMsg(string.format("{\"id\":%d,\"position\":%d,\"motor\":\"%s\"}",deviceID,position,motor))
+		local response = ELAN_DoHTTPExchange(socket, sHTTP, JSONMsg, 5000)
+		ELAN_Trace(string.format("Response: %s", response))
+		local p1, p2	
+		p1,p2 = response:find("200 OK")
+		if p1 ~= nil then
+			ELAN_Trace("Response OK")
+		else
+			ELAN_Trace(string.format("Error: %s", response))
+		end
+		ELAN_CloseSocket(socket)
+	end
